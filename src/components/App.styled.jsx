2195e92ee53f0febe471f73934d7a2d20d9f4f4c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-13 22:27:03 +0300

    Update App.styled.jsx

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import ScrollToTop from 'react-scroll-up';
 
 export const ModalImg = styled.img`
   width: 100%;
@@ -30,4 +31,19 @@ export const ModalDescr = styled.p`
   backdrop-filter: blur(5.5px);
 
   font-size: ${props => props.theme.fontSizes.l};
+`;
+
+export const BtnScrollToTop = styled(ScrollToTop)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 42px;
+  padding: 8px;
+  background-color: rgba(185, 228, 201, 0.5);
+  border-radius: 8px;
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  transform: scale(1);
 `;
