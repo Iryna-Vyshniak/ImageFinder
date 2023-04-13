2033d1d224eaed4815fcd29b9d90ebdc554fa457
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-13 22:25:43 +0300

    Create AlwaysScrollToBottom.jsx

---
+++ src/components/AlwaysScrollToBottom/AlwaysScrollToBottom.jsx
@@ -0,0 +1,7 @@
+import { useRef, useEffect } from 'react';
+
+export const AlwaysScrollToBottom = () => {
+  const elementRef = useRef();
+  useEffect(() => elementRef.current.scrollIntoView());
+  return <div ref={elementRef} />;
+};
