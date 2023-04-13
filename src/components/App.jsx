ac88cc4b0319ba390cdf53e83da80dbacd57b4e0
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-13 16:43:35 +0300

    add useState for page, add handleLoadMore and update handleSearchSubmit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 
 import { Searchbar } from './Searchbar/Searchbar';
-import ImageGallery from './ImageGallery/ImageGallery';
+import { ImageGallery } from './ImageGallery/ImageGallery';
 
 import { ToastContainer, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -10,12 +10,27 @@ import { Layout } from './Layout/Layout';
 
 export const App = () => {
   const [textSearch, setTextSearch] = useState('');
+  const [page, setPage] = useState(1);
+
+  const handleSearchSubmit = textSearch => {
+    setTextSearch(textSearch);
+    setPage(1);
+  };
+
+  // custom method to btn load
+  const handleLoadMore = () => {
+    setPage(prevSate => prevSate + 1);
+  };
 
   return (
     <>
-      <Searchbar onSubmit={setTextSearch} />
+      <Searchbar onSubmit={handleSearchSubmit} />
       <Layout>
-        <ImageGallery value={textSearch} />
+        <ImageGallery
+          value={textSearch}
+          page={page}
+          onLoadMore={handleLoadMore}
+        />
       </Layout>
       <ToastContainer transition={Slide} draggablePercent={60} />
       <GlobalStyle />
