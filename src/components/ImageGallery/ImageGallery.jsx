27273857aba8d43a35a6cd5dcde582d92bb2bcda
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-15 13:50:25 +0300

    add comments

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+
 import imagesAPI from 'services/getImages';
-import React from 'react';
+
 import { List } from './ImageGallery.styled';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 import { Loader } from '../Loader/Loader';
@@ -28,6 +30,7 @@ export const ImageGallery = ({ value, page, onLoadMore }) => {
     if (!value) {
       return;
     }
+    // при новому запиті - запит відбувається з 1 сторінки та попередній масив зображень обнуляється
     if (page === 1) {
       setImages([]);
     }
@@ -47,11 +50,12 @@ export const ImageGallery = ({ value, page, onLoadMore }) => {
       });
   }, [value, page, onLoadMore]);
 
+  // стартове дефолтне зображення в галереї до рендеру карток з зображеннями
   if (status === Status.IDLE) {
     return <InitialStateGallery text="Let`s find images together!" />;
   }
+  // при стадії очікування - дозавантаження картинок до вже знайдених + картинка-лоадер, поки дозавантажуються зображення
   if (status === Status.PENDING) {
-    // дозавантаження картинок до вже знайдених + картинка-лоадер, поки дозавантажуються зображення
     return (
       <>
         <List>
@@ -63,13 +67,16 @@ export const ImageGallery = ({ value, page, onLoadMore }) => {
       </>
     );
   }
+  // якщо запит відбувся з помилкою - рендериться дефолтне зображення з повідомленням помилки
   if (status === Status.REJECTED) {
     return <ImageErrorView message={error.message} />;
   }
+  // якщо при запиті зображення не знайдені - рендериться дефолтне зображення з повідомленням
   if (images.length === 0) {
     return <ImageErrorView message={errorMessages.imagesAPI} />;
   }
 
+  // якщо запит успішний  - рендериться галерея зображень
   if (status === Status.RESOLVED) {
     return (
       <>
@@ -78,6 +85,7 @@ export const ImageGallery = ({ value, page, onLoadMore }) => {
             <ImageGalleryItem key={image.id} item={image} />
           ))}
         </List>
+        {/* якщо при запиті зображення знайдені, запит не в стадії очікування та ще є сторінки з зображеннями - рендериться кнопка Load More*/}
         {images.length > 0 &&
           status !== Status.PENDING &&
           page <= totalPages && <Button onClick={onLoadMore}>Load More</Button>}
@@ -91,118 +99,6 @@ ImageGallery.propTypes = {
   page: PropTypes.number.isRequired,
   onLoadMore: PropTypes.func.isRequired,
 };
-
-// export default class ImageGallery extends Component {
-//   static propTypes = {
-//     value: PropTypes.string.isRequired,
-//   };
-
-//   state = {
-//     value: '',
-//     images: [],
-//     error: null,
-//     status: Status.IDLE,
-
-//     page: 1,
-//     totalPages: 0,
-
-//     isShowModal: false,
-//     modalData: { img: DefaultImg, tags: '' },
-//   };
-
-//   static getDerivedStateFromProps(nextProps, prevState) {
-//     if (prevState.value !== nextProps.value) {
-//       return { page: 1, value: nextProps.value };
-//     }
-//     return null;
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     const { page } = this.state;
-//     const prevValue = prevProps.value;
-//     const nextValue = this.props.value;
-
-//     if (prevValue !== nextValue || prevState.page !== page) {
-
-//       this.setState({ status: Status.PENDING });
-
-//       if (this.state.error) {
-//         this.setState({ error: null });
-//       }
-//       imagesAPI
-//         .getImages(nextValue, page)
-//         .then(images => {
-
-//           this.setState(prevState => ({
-//             images:
-//               page === 1 ? images.hits : [...prevState.images, ...images.hits],
-//             status: Status.RESOLVED,
-//             totalPages: Math.floor(images.totalHits / 12),
-//           }));
-//
-//         })
-//         .catch(error => this.setState({ error, status: Status.REJECTED }));
-//     }
-//   }
-
-//   handleLoadMore = () => {
-//     this.setState(prevState => ({ page: prevState.page + 1 }));
-//   };
-
-//   setModalData = modalData => {
-//     this.setState({ modalData, isShowModal: true });
-//   };
-
-//   handleModalClose = () => {
-//     this.setState({ isShowModal: false });
-//   };
-
-//   render() {
-//     const { images, error, status, page, totalPages, isShowModal, modalData } =
-//       this.state;
-
-//     if (status === 'idle') {
-//       return <InitialStateGallery text="Let`s find images together!" />;
-//     }
-//     if (status === 'pending') {
-//       return <Loader />;
-//     }
-//     if (status === 'rejected') {
-//       return <ImageErrorView message={error.message} />;
-//     }
-//     if (images.length === 0) {
-//       return (
-//         <ImageErrorView
-//           message={`Oops... there are no images matching your search... `}
-//         />
-//       );
-//     }
-
-//     if (status === 'resolved') {
-//       return (
-//         <>
-//           <List>
-//             {images.map(image => (
-//               <ImageGalleryItem
-//                 key={image.id}
-//                 item={image}
-//                 onImageClick={this.setModalData}
-//               />
-//             ))}
-//           </List>
-//           {images.length > 0 && status !== 'pending' && page <= totalPages && (
-//             <Button status="load" onClick={this.handleLoadMore}>
-//               Load More
-//             </Button>
-//           )}
-//           {isShowModal && (
-//             <Modal modalData={modalData} onModalClose={this.handleModalClose} />
-//           )}
-//         </>
-//       );
-//     }
-//   }
-// }
 
 // !for infinity scroll
 // const [offset, setOffset] = useState(0);
